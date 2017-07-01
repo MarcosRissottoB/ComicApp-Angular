3a67e5443c1f25d42b3686ff7f0d98dfd89576c3
Marcos Rissotto <marcosrissotto1@gmail.com>
2017-07-01 15:33:35 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//Ruotes
+import { APP_ROUTING } from './app.routes';
+
+//Service
+import {HeroesService} from './service/heroes.service';
+
+//Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../service/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:any[] = [];
+
+  constructor( private _heroesService:HeroesService ) {
+      console.log("constructor...");
+  }
+
+
+  ngOnInit() {
+
+      this.heroes = this._heroesService.getHeroes();
+
+      console.log(this.heroes)
+  }
+
+}
