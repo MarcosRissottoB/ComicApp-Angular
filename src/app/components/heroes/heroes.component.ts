5ca0af1c66c1846868e94cb168ec55a91acfc239
Marcos Rissotto <marcosrissotto1@gmail.com>
2017-07-08 11:41:21 -0300

    Commit Finish

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../service/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,16 @@ export class HeroesComponent implements OnInit {
 
   heroes:any[] = [];
 
-  constructor( private _heroesService:HeroesService ) {
-      console.log("constructor...");
+  constructor( private _heroesService:HeroesService,
+               private router:Router
+              ) {
   }
-
 
   ngOnInit() {
-
       this.heroes = this._heroesService.getHeroes();
-
-      console.log(this.heroes)
   }
 
+  verHeroe( idx:number ){
+    this.router.navigate( ['/heroe', idx]);
+  }
 }
--- src/app/service/heroes.service.ts
+++ src/app/service/heroes.service.ts
@@ -59,11 +59,30 @@ export class HeroesService {
 
 
   constructor() {
-    console.log("Servicio listo para usar.")
+    // console.log("Servicio listo para usar.")
   }
 
   getHeroes(){
     return this.heroes;
+  }
+
+  getHeroe( idx:string ){
+    return this.heroes[idx];
+  }
+
+  buscarHeroes( termino:string ):Heroe[]{
+
+    let heroesArr:Heroe[] = [];
+    termino = termino.toLowerCase();
+
+    for( let heroe of this.heroes ){
+
+      let nombre = heroe.nombre.toLowerCase();
+      if( nombre.indexOf( termino ) >= 0 ){
+        heroesArr.push( heroe )
+      }
+    }
+    return heroesArr;
   }
 
 }
